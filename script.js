96d68cf56a5719f910f4cd6b2eec67efb598eec4
darkosel3 <darkosel7@gmail.com>
2022-08-26 12:47:26 +0200

    Logic for AC

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const buttons = document.querySelectorAll( '.grid-item' );
 const current = document.querySelector( '.current-operand' );
 const previus = document.querySelector( '.previus-operand' );
+const reg = new RegExp( '^[0-9]$' );
+const regOp = new RegExp( '/[()+-*/.]/' );
+let calcPressed = false;
 
 
 const calculate = function () {
@@ -28,6 +31,15 @@ buttons.forEach( button => {
   button.addEventListener( 'click', function () {
     if ( this.innerText === 'AC' ) {
       allClear();
+    } else if ( reg.test( this.innerText ) ) {
+      if ( current.innerText === '0' ) {
+        current.innerText = '';
+        current.innerText += this.innerText;
+      } else {
+        current.innerText += this.innerText;
+      }
+    } else if ( regOp.test( this.innerText ) ) {
+      console.log( this.innerText );
     }
   } );
 
